e07728ef5d46072a140838eb5f819c437b2789cb
Sally Ross <46608728+SRAdda@users.noreply.github.com>
2019-07-18 16:39:04 -0700

    Add files via upload

---
+++ show.js
@@ -0,0 +1,11 @@
+let productions = [
+    { name:'Chicago', price:65, venue:'Paramount' },
+    { name:'White Christmas', price:75, venue:'Paramount' },
+    { name:'Brigadoon', price:85, venue:'5th Avenue' }
+];
+
+const getAll = () => {
+    return productions;
+}
+
+module.exports = { getAll }
