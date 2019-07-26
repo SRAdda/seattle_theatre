975a02a73aabd8f7afd7ae93d9bec37ef3a651ac
Sally <sraddabbo@hotmail.com>
2019-07-25 17:29:51 -0700

    HW2 (mostly) data methods

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 'use strict'
+
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const app = express();
+const show = require('./lib/show');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set loc for static files
 app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
+let handlebars = require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html', 
+defaultLayout: false}));
+app.set("view engine", ".html");
+
 // send static file as response
-app.get('/', (req, res) => {
-  console.log(req.query)
+app.get('/', (_req,res) => {
   res.type('text/html');
-  res.sendFile(__dirname + '/public/home2.html');
+  res.sendFile(__dirname + '/public/home.html');
 });
 
 // send plain text response
-app.get('/about', (req, res) => {
+app.get('/about', (_req,res) => {
   res.type('text/plain');
-  res.send('About ', + req.query.name+ '\'s page');
+  res.send('About page');
 });
 
 // handle form submission
 app.post('/detail', (req, res) => {
-    console.log(req.body.username)
-  res.type('text/plain');
-  res.send('Detail page');
+  console.log(req.body)
+  var found = show.getItem(req.body.title);
+  console.log(found)
+  res.render('detail', {
+    title: req.body.title,
+    result: found
+  });
 });
 
 // define 404 handler
-app.use( (req,res) => {
+app.use((_req,res) => {
   res.type('text/plain');
   res.status(404);
   res.send('404 - Not found');
